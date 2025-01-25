e2e62a917e73bf96f13ec89d2492568574a3443e
Tahsin Rahman <tr174@duke.edu>
2025-01-24 23:36:09 -0500

    fixed a few bugs

---
+++ README.md
@@ -0,0 +1,12 @@
+# Encrypt Text
+
+Simple implementation of a text encryption and decryption system written in Go. It uses a Caesar cipher-like mechanism to transform text based on a given key.
+
+---
+
+## Features
+
+- Encrypt text using a custom rotation-based cipher.
+- Decrypt text to recover the original message.
+- Supports only uppercase English alphabets.
+
--- main.go
+++ main.go
@@ -1,21 +1,20 @@
 package main
 
-import ("fmt"
-		"strings"
-
+import (
+	"fmt"
+	"strings"
 )
-
 
 const originalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func hashLetterFn(key int, letter string) (result string){
+func hashLetterFn(key int, letter string) (result string) {
 	runes := []rune(letter)
 	lastLetterKey := string(runes[len(letter)-key : len(letter)])
-	leftOverLetters := string(runes[0:len(letter)-key])
-	return fmt.Sprintf(('%s%s', lastLetterKey, leftOverLetters))
+	leftOversLetter := string(runes[0 : len(letter)-key])
+	return fmt.Sprintf(`%s%s`, lastLetterKey, leftOversLetter)
 }
 
-func encrypt(key int, plainText string) (result string){
+func encrypt(key int, plainText string) (result string) {
 	hashLetter := hashLetterFn(key, originalLetter)
 	var hashedString = ""
 	findOne := func(r rune) rune {
@@ -25,35 +24,32 @@ func encrypt(key int, plainText string) (result string){
 			hashedString = hashedString + string(hashLetter[letterPosition])
 			return r
 		}
-		return r 
+		return r
 	}
 	strings.Map(findOne, plainText)
 	return hashedString
 }
-
-func decrypt(key int, encryptedText string) (result string){
+func decrypt(key int, encrypttedText string) (result string) {
 	hashLetter := hashLetterFn(key, originalLetter)
 	var hashedString = ""
-	findOne := func(r rune) rune{
+	findOne := func(r rune) rune {
 		pos := strings.Index(hashLetter, string([]rune{r}))
-		if pos != -1{
+		if pos != -1 {
 			letterPosition := (pos + len(originalLetter)) % len(originalLetter)
 			hashedString = hashedString + string(originalLetter[letterPosition])
 			return r
 		}
 		return r
 	}
-
-	strings.Map(findOne, encryptedText)
+	strings.Map(findOne, encrypttedText)
 	return hashedString
 }
 
-func main(){
-	plainText :="HELLOWORLD"
-	fmt.Println("Plain Text", plaintext())
+func main() {
+	plainText := "HELLOWORLD"
+	fmt.Println("Plain Text", plainText)
 	encrypted := encrypt(5, plainText)
-	fmt.Println("Encrypted text", encrypted)
+	fmt.Println("Encrypted", encrypted)
 	decrypted := decrypt(5, encrypted)
 	fmt.Println("Decrypted", decrypted)
-
 }
