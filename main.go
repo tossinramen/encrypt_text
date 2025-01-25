f91a4b9d202fa881fb850dabecb32c13bfd39cb0
Tahsin Rahman <tr174@duke.edu>
2025-01-24 22:51:17 -0500

    initialized functions

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import(
+
+)
+
+
+func encrypt(key int, plainText string) (result string){
+
+}
+
+func decrypt(key int, encryptedText string) (result string){
+	
+}
+
+func main(){
+	plainText :="HELLOWORLD"
+	fmt.Println("Plain Text", plaintext())
+	encrypted := encrpyt(5, plainText)
+}
