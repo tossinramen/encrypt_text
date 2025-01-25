cb28642d34462c517f545bb9e2a083c14313e137
Tahsin Rahman <tr174@duke.edu>
2025-01-24 23:02:45 -0500

    hashletterfn

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
-import(
+import "fmt"
 
-)
+const originalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+func hashLetterFn(key int, letter string) (result string){
+	runes := []rune(letter)
+	lastLetterKey := string(runes[len(letter)-key : len(letter)])
+	leftOverLetters := string(runes[0:len(letter)-key])
+	return fmt.Sprintf(('%s%s', lastLetterKey, leftOverLetters))
+}
 
 func encrypt(key int, plainText string) (result string){
+	hashLetter := hashLetterFn(key, originalLetter)
+	var hashedString = ""
+	findOne := func(r rune) rune {
+
+	}
+	strings.Map(findOne, plainText)
 
 }
 
 func decrypt(key int, encryptedText string) (result string){
-	
+
 }
 
 func main(){
 	plainText :="HELLOWORLD"
 	fmt.Println("Plain Text", plaintext())
-	encrypted := encrpyt(5, plainText)
+	encrypted := encrypt(5, plainText)
+	fmt.Println("Encrypted text", encrypted)
+	decrypted := decrypt(5, encrypted)
+	fmt.Println("Decrypted", decrypted)
+
 }
